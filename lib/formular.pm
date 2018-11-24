b6c628630bc459649d02dba26fc0c1688b2e1c62
raschu <mail@ralphschuler.ch>
2018-11-24 07:42:25 +0100

    Mail umformatiert

--- lib/formular.pm
+++ lib/formular.pm
@@ -24,22 +24,24 @@ post '/kontaktpost' => sub {
 		my $ip = $ENV{'HTTP_X_FORWARDED_FOR'};
 		
 		$ip = 'localhost' unless (defined $ip);
+				
+		open(DAT, ">>$appdir/data/$ip.$now.txt");
+		print DAT "Name         : $name\n";
+		print DAT "Email        : $email\n";
+		print DAT "IP           : $ip\n";
+		print DAT "Timestamp    : $now\n";		
+		print DAT "Kommentar    :\n";
+		print DAT "-------------------------------\n";
+		print DAT "$comment\n";
+		print DAT "-------------------------------\n";
+		close(DAT);
 		
 		$comment =~ s/\n/<br>/g;
 		$comment =~ s/\'/''/g;
 		
-		open(DAT, ">>$appdir/data/$ip.$now.txt");
-		print DAT "name: $name\n";
-		print DAT "email: $email\n";
-		print DAT "ip: $ip\n";
-		print DAT "now: $now\n";		
-		print DAT "-------------------------------\n";
-		print DAT "comment: $comment\n";
-		print DAT "-------------------------------\n";
-		close(DAT);
-		
 		my $crypt = Crypt::Lite->new( debug => 0, encoding => 'hex8' );
 		my $decrypted_mail1 = $crypt->decrypt('43d8f6b5076e591c91aba97d7f50058da6f54e7b551188afa96d73557980fbb76828004180f4b4372f0142d3fab03b2c084784f6e23b2f0f13d4a7e53e', 'pf08d4');
+		#todo: weitere Empfängeradresse definieren, evtl sogar Trello Adresse: ralphschuler1xy99w5@boards.trello.com
 		
 		system("mutt -s \"Kontakt ISAF2020\" $decrypted_mail1 < $appdir/data/$ip.$now.txt");
 		
